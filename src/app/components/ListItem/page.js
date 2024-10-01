0f5d4974d1ef688943ebb189fbc5eef623555863
duminoi <dminso1koaiso2@gmail.com>
2024-10-02 01:08:16 +0700

    u

--- src/app/components/ListItem/page.js
+++ src/app/components/ListItem/page.js
@@ -5,6 +5,7 @@ import Link from "next/link";
 
 export default function ListItem({ id, category, price }) {
   // console.log("image", category.image);
+  console.log("category", category);
 
   return (
     <div className=" flex flex-col justify-center items-center">
@@ -13,16 +14,42 @@ export default function ListItem({ id, category, price }) {
           priority
           width={300}
           height={500}
-          src={category.image ? category.image : product1}
+          src={category?.image ? category?.image : product1}
           alt="Product image"
           style={{ width: "100%", height: "auto" }}
         ></Image>
       </Link>
       <div className="text-3xl font-bold">
-        {category.name ? category.name : "Product"}
+        {category?.name ? category?.name : "Product"}
       </div>
 
       <div className="text-2xl  text-red-700">Price: {price} $</div>
     </div>
   );
 }
+
+// import Image from "next/image";
+// import React from "react";
+// import product1 from "@/app/images/product1.jpg";
+// import Link from "next/link";
+
+// export default function ListItem({ id, category = {}, price }) {
+//   return (
+//     <div className="flex flex-col justify-center items-center">
+//       <Link href={`components/ListItem/${id}`}>
+//         <Image
+//           priority
+//           width={300}
+//           height={500}
+//           src={category?.image ? category.image : product1}
+//           alt={category?.name || "Product image"}
+//           style={{ width: "100%", height: "auto" }}
+//         />
+//       </Link>
+//       <div className="text-3xl font-bold">
+//         {category?.name ? category.name : "Product"}
+//       </div>
+//       <div className="text-2xl text-red-700">Price: {price} $</div>
+//     </div>
+//   );
+// }
