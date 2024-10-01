d50d84e0b641f395cdb08aa741cf956a0a98998c
duminoi <dminso1koaiso2@gmail.com>
2024-10-01 20:27:21 +0700

    u

--- src/app/store/store.js
+++ src/app/store/store.js
@@ -1,8 +1,8 @@
-import { configureStore } from "@reduxjs/toolkit";
-import productsReducer from "./productsSlice";
+// import { configureStore } from "@reduxjs/toolkit";
+// import productsReducer from "./productsSlice";
 
-export const store = configureStore({
-  reducer: {
-    products: productsReducer,
-  },
-});
+// export const store = configureStore({
+//   reducer: {
+//     products: productsReducer,
+//   },
+// });
